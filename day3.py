7bf7c4056248bbd906588c3c62b63cd9d6174e28
PS <cs2398136@gmail.com>
2024-05-14 15:13:56 +0530

    grtg

---
+++ day3.py
@@ -0,0 +1,67 @@
+lst =[25,41,65,'hello',True]
+# print(lst)
+# print(lst[1:5])
+# lst[1]=500
+# print(lst)
+lst.insert(1,'jecrc')             #to insert at any position
+# lst.pop()                        #remove from list
+# print(len(lst))                  #how many elements in list 
+lst.append(100)                  #insert in list
+print(lst)
+# lst =[25,41,65,'hello',True]
+# mess=lst[4]
+# print(mess[-1])
+# print(lst[4][-1])
+# lst =[25,41,65,25.3,[100,200,300]]
+# print(lst[4][1])
+# lst[4][1]=1000                   #to overwrite
+# print(lst)
+# lst[1]=50
+# print(lst)
+# tpl=(10,20,30,40,57.34,'jecrc',True)
+# tpl[1]=200                               #will give error coz tuple does not support item insertion
+# print(tpl)
+# print(tpl[1])
+# print(tpl)
+# print(type(tpl))
+# college_name=('jecrc',)                   #if we want to create tuple with 1 element we have to put comma at last
+# print(type(college_name))
+# college_names="jecrc","poornima","vanasthali"
+# print(type(college_names))
+# print(college_names)
+# lst=[12,14,25,63,85,95]
+# print(lst)
+# lst.remove(63)                           #to remove from any position coz pop perform at last
+# print(lst)
+# name="jecrc"
+# surname="foundation"
+# roll_no=100
+# # print(name+surname)                             #merge
+# # print(name *2)                                 #replication
+# print(name+roll_no)                               #error coz datatype should be same to perform these operations
+
+# tpl2=(10,20,30,40)
+# print(tpl2*2)
+# lst=[2,44,23,24]
+# print(lst*2)
+
+# lst1=[1,2,3,4,5]
+# lst2=[6,7,8,9,10]
+# lst1.append(lst2)
+# lst1.extend(lst2)
+# print(lst1)
+
+# key,values
+# dt={"name":"rohit kumar","branch":'cse',"rpll_no":1}
+# print(dt)
+# print(type(dt))
+# print(dt["name"])
+# print(dt.keys())
+# print(dt.values())
+
+# dt={"name":"rohit kumar","branch":'cse',"rpll_no":1}
+# dt={0:'rohit kumar',1:"cse",2:101}
+# print(dt)
+
+# class_data={"name":["a","b","c","d"],"marks":[2,4,3,5]}
+# print(class_data="name")
