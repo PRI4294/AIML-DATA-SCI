64b9ea06bedce151cc318ee46fc9148a34297c21
PS <cs2398136@gmail.com>
2024-05-14 15:10:59 +0530

    khdkfh

---
+++ day2.py
@@ -0,0 +1,46 @@
+# var_1 = 10
+# var_2 = 25.21
+# var_3 = True
+# var_4 = "upflairs"
+# var_5 = 'JECRC'
+# var_6 = """Jecrc foundation"""
+# print(var_1)
+# print(var_2)
+# print(var_3)
+# print(var_4)
+# print(var_5)
+# print(var_6)
+# print()
+# print(type(var_1))
+
+#put string in 3 line
+# print("""hello my name is priyanshi
+#       i am from it branch
+#       of jecrc foundation""")
+
+# college="jecrc foundation"
+# print(college.upper())
+# print(college.lower())
+# print(len(college))
+# print(college.count('c'))
+# print(college.endswith('n'))
+ 
+#removing space
+# college =" jecrc foundation "
+# print(college.strip())
+# print(college.lstrip())
+# print(college.rstrip())
+
+#indexing    
+# print(college[6])
+# print(college[0:6])
+# college='jecrc'
+# print(college[3:5])
+# print(college[0:5:1])
+# print(college[0:5:2])       # 2 -> jump
+# print(college[-2:])
+# print(college[::-1])        #slicing reverse print
+# print(college[4::-1])
+# college='JECRC jaipur rajasthan'
+# print(college[-5:])
+# print(college[-1])
